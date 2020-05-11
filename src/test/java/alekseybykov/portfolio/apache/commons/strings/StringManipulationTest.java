9971a1e42654e7c4d9e30d756db978747cb9992e
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-05-11 06:34:16 +0400

    Added examples of making string capitalized.

--- src/test/java/alekseybykov/portfolio/apache/commons/strings/StringManipulationTest.java
+++ src/test/java/alekseybykov/portfolio/apache/commons/strings/StringManipulationTest.java
@@ -1,18 +1,42 @@
 package alekseybykov.portfolio.apache.commons.strings;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringManipulationTest {
 
+	private final String string = "this is an Example of a simple string";
+
 	@Test
 	public void testAbbreviateString() {
-		String string = "This is an example of a simple string";
+		assertEquals("this is...", StringUtils.abbreviate(string, 10));
+		assertEquals("...Example of ...", StringUtils.abbreviate(string, 11, 17));
+		assertEquals("this is an [censored]ple string", StringUtils.abbreviateMiddle(string, "[censored]", 31));
+	}
 
-		assertEquals("This is...", StringUtils.abbreviate(string, 10));
-		assertEquals("...example of ...", StringUtils.abbreviate(string, 11, 17));
-		assertEquals("This is an [censored]ple string", StringUtils.abbreviateMiddle(string, "[censored]", 31));
+	@Test
+	public void testCapitalizeString() {
+		assertEquals("This is an Example of a simple string", StringUtils.capitalize(string));
+		assertEquals("This Is An Example Of A Simple String", WordUtils.capitalize(string));
+		assertEquals("This Is An Example Of A Simple String", WordUtils.capitalizeFully(string));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
