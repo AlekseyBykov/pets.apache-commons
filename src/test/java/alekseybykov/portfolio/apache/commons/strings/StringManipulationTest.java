39d1f0ac4f4a711f84082c1b62f95421164a7b5d
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-05-11 06:20:01 +0400

    Added additional examples of abbreviating.

--- src/test/java/alekseybykov/portfolio/apache/commons/strings/StringManipulationTest.java
+++ src/test/java/alekseybykov/portfolio/apache/commons/strings/StringManipulationTest.java
@@ -8,8 +8,11 @@ import static org.junit.Assert.assertEquals;
 public class StringManipulationTest {
 
 	@Test
-	public void testAbbreviateLongString() {
+	public void testAbbreviateString() {
 		String string = "This is an example of a simple string";
+
 		assertEquals("This is...", StringUtils.abbreviate(string, 10));
+		assertEquals("...example of ...", StringUtils.abbreviate(string, 11, 17));
+		assertEquals("This is an [censored]ple string", StringUtils.abbreviateMiddle(string, "[censored]", 31));
 	}
 }
